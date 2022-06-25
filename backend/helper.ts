6541f074d98c05b4a51282189ab1acee2238ceff
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2022-06-25 19:32:31 +0200

    update

--- backend/helper.ts
+++ backend/helper.ts
@@ -14,9 +14,25 @@ export function getDirname(url: string) {
     return path;
 }
 
+export function getDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = `0${date.getMonth() + 1}`.slice(-2);
+    const day = `0${date.getDate()}`.slice(-2);
+    return `${year}-${month}-${day}`;
+}
+
+export function getTime() {
+    const date = new Date();
+    const hh = `0${date.getHours()}`.slice(-2);
+    const mm = `0${date.getMinutes()}`.slice(-2);
+    const ss = `0${date.getSeconds()}`.slice(-2);
+    return `${hh}:${mm}:${ss}`;
+}
+
 export function getJsonResponse(
     success: boolean,
-    result: VaultEntries,
+    result: VaultEntries | Record<string, string> | null,
     message?: number
 ): JsonResponse {
     return {
